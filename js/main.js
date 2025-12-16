65fe3ddfecaaaf6a635c32cf1b8a931fa0364c2d
Rychoz <r.zajul@gmail.com>
2025-12-16 10:12:09 +0100

    wykonanie zadania

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let number = 12;
+
+
+console.log("The number is:", number);
+
+if (number > 10) {
+    console.log("Podana liczba jest większa od 10");
+} else {
+    console.log("Podana liczba nie jest wiuększa od 10");
+}
+
+
+
